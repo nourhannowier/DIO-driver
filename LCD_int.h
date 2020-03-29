77daf5525b7ef247ce06ac885e1697ddcf873c39
nourhannowier <nourhanahmed100@gmail.com>
2020-03-29 16:16:30 +0200

    Add files via upload

---
+++ LCD_int.h
@@ -0,0 +1,37 @@
+/*********************************************************/
+/***********  Author       :NOURHAN NOWIER        ********/
+/***********  Version      :V01                   ********/
+/***********  Date         :26/3/2020             ********/
+/***********  Description  :LCD_INTERFACE FUNCS   ********/
+/*********************************************************/ 
+
+
+
+/*********************************************************/
+/*** guard of file will call one time in .c    ***********/
+/*********************************************************/
+
+#ifndef _LCD_INT_H_
+#define _LCD_INT_H_
+
+/****                  CONTROL                       *****/
+#define  LCD_CONTROL          PORTA
+#define  RS_PIN               PIN0
+#define  RW_PIN               PIN1
+#define  E_PIN                PIN2
+
+
+/****                  DATA                          *****/
+#define  LCD_DATA             PORTB
+
+
+ERROR_enuStatus     LCD_enuInit(void);
+
+ERROR_enuStatus     LCD_enuSendCMD(  u8 Copy_u8CMD  ); 
+
+ERROR_enuStatus     LCD_enuSendData( u8 Copy_u8Data ); 
+
+
+
+
+#endif
